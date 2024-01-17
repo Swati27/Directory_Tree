a9c80414fc85fb2533c2ec190d3d2728ac4a47a1
Swati <agrswati2794@gmail.com>
2024-01-16 16:46:03 -0800

    logic to render button if children + if expanded then - sign

--- src/Entry.js
+++ src/Entry.js
@@ -5,9 +5,14 @@ function Entry({entry, depth}) {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
     <>
+    {entry?.children? (
+       <div>
+       <button style={{paddingLeft: `${depth * 10}px`, background: 'none'}} onClick={()=>setIsExpanded(!isExpanded)}>{isExpanded? `- ${entry?.name}` : `+ ${entry?.name}`}</button>
+       </div>
+    ):
     <div>
-    <button style={{paddingLeft: `${depth * 10}px`, background: 'none'}} onClick={()=>setIsExpanded(!isExpanded)}>{entry.children? `+ ${entry?.name}` : entry?.name}</button>
-    </div>
+    <div style={{paddingLeft: `${depth * 10}px`}}>{entry?.name}</div>
+    </div>}
     {isExpanded && entry?.children?.map((entry)=>
     {
         return <Entry entry = {entry} depth = {depth + 1}></Entry>// used recursion here
